b905bc92d5ba021375ca4c7d775cabc3db1b69ea
engines2053@gmail.com <116022211+EnginesTheStone@users.noreply.github.com>
2025-03-08 14:59:29 +0000

    tests

--- cards/urls.py
+++ cards/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
+def redirect_to_home(request):
+    return redirect ('/cards/')
+
+
 
 
 urlpatterns = [
     #URL links#
-    path("cards/", views.home, name = "home"),
+    path('', redirect_to_home),
+    path("cards/", include('cards.urls')),#views.home, name = "home"#),
     path("buy/", views.buy, name="buy" ),
     path("checkout/", views.checkout, name = "checkout"),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
